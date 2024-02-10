bd4ddf152e482e358c17b90d16d3741e61a16401
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-10 21:38:07 +0100

    first commit

---
+++ Day1.py
@@ -0,0 +1,9 @@
+#A program that suggests band names based on two questions.. enjoy :)
+#This is the first program I'll deploy on my 100 days of python journey
+
+print("Welcome to Mia's Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of a pet you own/owned\n")
+
+print("Here's a name your band should try")
+print(city_name + " " + pet_name)
