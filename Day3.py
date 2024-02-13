25eb6cd385b719876e082bc7bcad7d3c747bcfbf
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-13 21:42:11 +0100

    Day 3

---
+++ Day3.py
@@ -0,0 +1,14 @@
+
+print('''
+         __________
+        /\____;;___\\
+       | /         /
+       `. ())oo() .
+        |\(%()*^^()^\\
+       %| |-%-------|
+      % \ | %  ))   |
+      %  \|%________|      
+''')
+
+print("\nWelcome to the Treasure Island.")
+print("Your mission is to find the treasure\n")
