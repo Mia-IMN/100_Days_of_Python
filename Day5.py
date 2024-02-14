fb110cc6f88a5c1979afb1cf0db4fe232e3ee1fd
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-14 21:01:18 +0100

    Day 5

---
+++ Day5.py
@@ -0,0 +1,27 @@
+
+
+# Welcome message
+print("Welcome to the PyPassword Generator!")
+
+# Getting user's preferences
+password_length = int(input("How many letters would you like in your password?\n"))
+num_of_symbols = int(input("How many symbols would you like?\n"))
+num_of_numbers = int(input("How many numbers would you like?\n"))
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+             'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+             'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# Generating the random password
+
+
+
+
+
+print(numbers)
+print(f"Here's your password: {password}")
