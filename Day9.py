ca15e3fabef6667f086739ec430af8f891ec0cf3
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-19 18:10:18 +0100

    Day 9: Fifth commit

--- Day9.py
+++ Day9.py
@@ -13,10 +13,8 @@ print(''''
 # Introductory message
 print("Welcome to the secret aution program")
 
-bidders = []
-num = 0
 highest_bid = 0
-bidder_number = 0
+bidder_name = "Error: Nothing to see here"
 other_bidders = True
  
 while other_bidders == True:
@@ -25,13 +23,11 @@ while other_bidders == True:
     bid = int(input("What is your bid?: "))
     if bid > highest_bid:
         highest_bid = bid
-        bidder_number = num
+        bidder_name = name
     other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ") 
     if other_bidders == "yes":
         other_bidders = True
     else:
         other_bidders = False
-    num += 1
-    bidders.append({"name": name, "bid": bid})
-        
-print(bidders)
+            
+print(f"The winner is {bidder_name} with a bid of ${highest_bid}")
