81e24b486b7acd3e347893aa8cf9e6d51f6765a8
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-19 17:21:48 +0100

    Day 9: Third commit

--- Day9.py
+++ Day9.py
@@ -9,3 +9,13 @@ print(''''
                 '.__'-'__.'
                     '''
 '')
+
+# Introductory message
+print("Welcome to the secret aution program")
+bidder_1 = int(input("What is your name?: "))
+bid_1 = int(input("What is your bid?: "))
+other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
+
+bidder_2 = int(input("What is your name?: "))
+bid_2 = int(input("What is your bid?: "))
+other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ")
