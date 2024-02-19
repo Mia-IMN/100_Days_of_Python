01e35ef49247093540a9553af77d1af1a604f652
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-19 18:02:41 +0100

    Day 9: Fourth commit

--- Day9.py
+++ Day9.py
@@ -15,12 +15,17 @@ print("Welcome to the secret aution program")
 
 bidders = []
 num = 0
+highest_bid = 0
+bidder_number = 0
 other_bidders = True
  
 while other_bidders == True:
     os.system('cls')
     name = input("What is your name?: ")
     bid = int(input("What is your bid?: "))
+    if bid > highest_bid:
+        highest_bid = bid
+        bidder_number = num
     other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ") 
     if other_bidders == "yes":
         other_bidders = True
@@ -28,6 +33,5 @@ while other_bidders == True:
         other_bidders = False
     num += 1
     bidders.append({"name": name, "bid": bid})
-
         
 print(bidders)
