5b896fab617b7fe9b4331c1f3378bbc8607a674e
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-02-20 21:28:05 +0100

    Day 10: third commit

--- Day10.py
+++ Day10.py
@@ -19,14 +19,29 @@ print(
     """"
 )
 
+def calculator_function(num1, num2, operator):
+    """Calculate the value of two inputs
+    based on a chosen arithmetic symbol"""
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else: 
+        return "Operator not recognized"
+
+should_continue = True
 
 while should_continue == True
     first_num = int(input("What's the first number?: "))
-    print("+\n-\n*\n\\\n")
+    print("+\n-\n*\n/\n")
     operator = input("Pick an operation: ")
     next_num = int(input("What's the next number?: "))
 
-    
+
 
     next_operation = input("Type 'y' to continue calculating with 10.0, or type 'n' to start a new calculation: ")
     if next_operation == 'n':
