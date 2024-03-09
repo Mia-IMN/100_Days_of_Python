fc8e9dffda5a619802549645d8e8e89b71c0ef9b
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-03-09 08:52:53 +0100

    Day 14: 2nd Commit

--- Day14.py
+++ Day14.py
@@ -34,4 +34,52 @@ data = [
     'description': 'Footballer',
     'country': 'Portugal'
   },
+  {
+    'name': 'Ariana Grande',
+    'follower_count': 183,
+    'description': 'Musician and Actress',
+    'country': 'United States'
+  },
+  {
+    'name': 'Dwayne Johnson',
+    'follower_count': 181,
+    'description': 'Actor and professional wrestler',
+    'country': 'United States'
+  },
+  {
+    'name': 'Taylor Swift',
+    'follower_count': 131,
+    'description': 'Musician',
+    'country': 'United States'
+  },
+  {
+    'name': 'Kendall Jenner',
+    'follower_count': 127,
+    'description': 'Reality TV personality and model',
+    'country': 'United States'
+  },
+  {
+    'name': 'Jennifer Lopez',
+    'follower_count': 119,
+    'description': 'Musician and actress',
+    'country': 'United States'
+  },
+  {
+    'name': 'Nicki Minaj',
+    'follower_count': 113,
+    'description': 'Musician',
+    'country': 'Trinidad and Tobago'
+  },
+  {
+    'name': 'National Geographic',
+    'follower_count': 135,
+    'description': 'Magazine',
+    'country': 'United States'
+  },
+  {
+    'name': 'Justin Bieber',
+    'follower_count': 133,
+    'description': 'Social media platform',
+    'country': 'Canada'
+  }
 ]
