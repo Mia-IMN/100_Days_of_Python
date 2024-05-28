9ae1c79143ac31cdca4a28fcc8282fbc278b8caa
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-05-28 23:16:59 +0100

    Day 7: update 1

--- Day7.py
+++ Day7.py
@@ -1,1 +1,14 @@
 # coding Hangman game
+
+# Steps
+
+word_list = ["ardvark", "baboon", "camel"]
+
+#TODO1 - Randomly choose a word from the word_list and assign it to a 
+# variable called chosen_word
+
+#TODO2 - Ask the user to guess a letter and assign their answer to a 
+# variable called guess. Make guess lowercase.
+
+#TODO3 - Check if the user guessed (guess) is one of the letters in 
+# chosen_word
