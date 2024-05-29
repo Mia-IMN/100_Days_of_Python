8407a2acd83e52f452fb3d45ad2ce20dca547476
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-05-29 23:42:35 +0100

    Day 7: new commit

--- Day7.py
+++ Day7.py
@@ -2,28 +2,24 @@
 
 import random
 
-# Steps
+
+word_list = ["ardvark", "baboon", "camel"]
+
+# Steps - 1
 
 #TODO1 - Randomly choose a word from the word_list and assign it to a 
 # variable called chosen_word
 
+chosen_number = random.choice(word_list)
+chosen_word = word_list[chosen_number]
+
 #TODO2 - Ask the user to guess a letter and assign their answer to a 
 # variable called guess. Make guess lowercase.
 
+guess = input("Guess a letter: ").lower
+
 #TODO3 - Check if the user guessed (guess) is one of the letters in 
 # chosen_word
-
-word_list = ["ardvark", "baboon", "camel"]
-
-guess = input("Guess a letter: ")
-
-chosen_number = random.choice(word_list)
-
-chosen_word = word_list[chosen_number]
-
-print (chosen_number)
-
-print(chosen_word)
 
 for i in chosen_word:
     if guess == i:
